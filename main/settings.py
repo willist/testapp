ea85483a41fc19ee1213324ee81df038ad652120
Tim Willis <schadenfreude328@gmail.com>
2012-04-12 19:48:20 -0400

    renaming primary project

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,4 @@
-# Django settings for willowbiz project.
+# Django settings for main project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -100,10 +100,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'willowbiz.urls'
+ROOT_URLCONF = 'main.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'willowbiz.wsgi.application'
+WSGI_APPLICATION = 'main.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
--- main/urls.py
+++ main/urls.py
@@ -7,8 +7,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'willowbiz.views.home', name='home'),
-    # url(r'^willowbiz/', include('willowbiz.foo.urls')),
+     url(r'^$', 'main.views.home', name='home'),
+    # url(r'^main/', include('willowbiz.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
